1b329ebd50d2674e9defd4525122ee0e84253428
AndresMpa <andresprieto2703@gmail.com>
2020-12-05 15:07:54 -0500

    Agregre atributos a renderizar desde el componente del footer

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,43 +1,49 @@
 <template>
-
     <footer>
         <div class="container-fluid text-white bg-dark">
             <div class="row p-5 mb-5">
                 <div class="col-6">
-                    <a class="mx-auto d-block text-center" href="https://github.com/Grupo-91-MinTic/semana-1-91.git" target="blank">
+                    <a class="mx-auto d-block text-center" :href="repository" target="blank">
                         <img src="https://img.icons8.com/dusk/64/000000/github.png"/> <br> <strong>Link del repositorio</strong>
                     </a>
                     <hr>
-                    <p class="text-center">Este proyecto se creo para sustentar lo aprendido en el sprint II del ciclo 3, usando Vue, Boostrap, JS</p>
+                    <p class="text-center">{{description}}</p>
                 </div>
                 <div class="col-6 badge">
                     <a href="https://www.linkedin.com/in/andr%C3%A9s-m-prieto-263a511b7/" target="blank">
-                        <img class="text-center" src="https://img.icons8.com/dusk/64/000000/linkedin.png"/>Andrés Manuel Prieto Álvarez (Main frontend developer)
+                        <img class="text-center" src="https://img.icons8.com/dusk/64/000000/linkedin.png"/>{{names[0]}} {{charges[0]}}
                     </a>
                     <br>
                     <a href="https://github.com/AndresMpa" target="blank">
-                        <img class="text-center" src="https://img.icons8.com/dusk/64/000000/github.png"/>Andrés Manuel Prieto Álvarez (Main frontend developer)
+                        <img class="text-center" src="https://img.icons8.com/dusk/64/000000/github.png"/>{{names[0]}} {{charges[0]}}
                     </a>
                     <hr>
                     <a href="https://github.com/carSneider99" target="blank">
-                        <img class="text-center" src="https://img.icons8.com/dusk/64/000000/github.png"/>Carlos Sneider Soto Sogamoso(Main backend developer)
+                        <img class="text-center" src="https://img.icons8.com/dusk/64/000000/github.png"/>{{names[1]}} {{charges[0]}}
                     </a>
                     <hr>
                     <a href="https://github.com/AlejoCM12" target="blank">
-                        <img class="text-center" src="https://img.icons8.com/dusk/64/000000/github.png"/>Harold Alejandro Caicedo (Frontend developer)
+                        <img class="text-center" src="https://img.icons8.com/dusk/64/000000/github.png"/>{{names[2]}} {{charges[0]}}
                     </a>
                     <hr>
                     <a href="https://github.com/JuanSebOrduz" target="blank">
-                        <img class="text-center" src="https://img.icons8.com/dusk/64/000000/github.png"/>Juan Sebastion Ortiz (Backend developer)
+                        <img class="text-center" src="https://img.icons8.com/dusk/64/000000/github.png"/>{{names[3]}} {{charges[0]}}
                     </a>
                 </div>
             </div>
         </div>        
     </footer>
-
 </template>
 <script>
 export default {
-    
+    name: "FooterSection",
+    data(){
+        return{
+            repository: 'https://github.com/Grupo-91-MinTic/semana-2-91',
+            description: 'Este proyecto se creo para sustentar lo aprendido en el sprint II del ciclo 3, usando Vue, Boostrap, JS',
+            names: ['Andrés Manuel Prieto Álvarez', 'Carlos Sneider Soto Sogamoso', 'Harold Alejandro Caicedo', 'Juan Sebastion Ortiz'],
+            charges: ['(Main frontend developer)', '(Main backend developer)', '(Frontend developer)', '(Backend developer)']
+        }
+    }
 }
 </script>
