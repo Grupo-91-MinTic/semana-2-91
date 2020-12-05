5db286fb69c5eb20fe6a63a7a32375087baa09c0
AndresMpa <andresprieto2703@gmail.com>
2020-12-05 14:36:50 -0500

    Footer completado

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <footer>
+        <div class="container-fluid text-white bg-dark">
+            <div class="row p-5 mb-5">
+                <div class="col-6">
+                    <a class="mx-auto d-block text-center" href="https://github.com/Grupo-91-MinTic/semana-1-91.git" target="blank">
+                        <img src="https://img.icons8.com/dusk/64/000000/github.png"/> <br> <strong>Link del repositorio</strong>
+                    </a>
+                    <hr>
+                    <p class="text-center">Este proyecto se creo para sustentar lo aprendido en el sprint II del ciclo 3, usando Vue, Boostrap, JS</p>
+                </div>
+                <div class="col-6 badge">
+                    <a href="https://www.linkedin.com/in/andr%C3%A9s-m-prieto-263a511b7/" target="blank">
+                        <img class="text-center" src="https://img.icons8.com/dusk/64/000000/linkedin.png"/>Andrés Manuel Prieto Álvarez (Main frontend developer)
+                    </a>
+                    <br>
+                    <a href="https://github.com/AndresMpa" target="blank">
+                        <img class="text-center" src="https://img.icons8.com/dusk/64/000000/github.png"/>Andrés Manuel Prieto Álvarez (Main frontend developer)
+                    </a>
+                    <hr>
+                    <a href="https://github.com/carSneider99" target="blank">
+                        <img class="text-center" src="https://img.icons8.com/dusk/64/000000/github.png"/>Carlos Sneider Soto Sogamoso(Main backend developer)
+                    </a>
+                    <hr>
+                    <a href="https://github.com/AlejoCM12" target="blank">
+                        <img class="text-center" src="https://img.icons8.com/dusk/64/000000/github.png"/>Harold Alejandro Caicedo (Frontend developer)
+                    </a>
+                    <hr>
+                    <a href="https://github.com/JuanSebOrduz" target="blank">
+                        <img class="text-center" src="https://img.icons8.com/dusk/64/000000/github.png"/>Juan Sebastion Ortiz (Backend developer)
+                    </a>
+                </div>
+            </div>
+        </div>        
+    </footer>
+
+</template>
+<script>
+export default {
+    
+}
+</script>
