8b23435bc2ada6f06720ef43770741296ffb6691
AndresMpa <andresprieto2703@gmail.com>
2020-12-05 23:46:40 -0500

    Correcciones

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,7 +1,7 @@
 <template>
     <article id="team">
         <div class="card badge-info container-fluid mt-5 pt-5" style="width: 18rem;">
-            <img :src="member.image​" class="card-img-top" :alt="member.​nombre​">
+            <img :src="member.image​" class="card-img-top" :alt="member.codigo">
             <div class="card-body text-center">
                 <h3 class="card-title">{{member.​nombre​}}</h3>
                 <h6 class="card-text">{{member.rol}}</h6>
