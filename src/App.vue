4c8432b04328b01441ab03ea72637d922e32657d
AndresMpa <andresprieto2703@gmail.com>
2020-12-06 01:08:49 -0500

    Se reparo el reto

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ import HeaderSection from './components/HeaderSection.vue';
 
 
 export default {
-  ​nombre​: 'App',
+  name: 'App',
   components: {
     Services,
     NewsSection,
@@ -35,31 +35,32 @@ export default {
     return{
       team: [
         {
-        codigo​: 1,
-        ​nombre​: 'Andrés Manuel Prieto Álvarez',
-        ​descripcion​: 'Yo soy Andrés M. Prieto; tengo 6 años de experiencia en TI, 3 años de desarrollo Frontend, 2 años de desarrollo de bases de datos relacionales, 1 año en desarrollo de arquitecturas, 1 año de metodología Scrum; soy estudiante de ingeniería en sistemas y computación de la UTP',
+        codigo: 1,
+        nombre: 'Andrés Manuel Prieto Álvarez',
+        descripcion: 'Yo soy Andrés M. Prieto; tengo 6 años de experiencia en TI, 3 años de desarrollo Frontend, 2 años de desarrollo de bases de datos relacionales, 1 año en desarrollo de arquitecturas, 1 año de metodología Scrum; soy estudiante de ingeniería en sistemas y computación de la UTP',
         rol: '(Main Frontend Developer)',
-        image​: 'https://avatars3.githubusercontent.com/u/39841241?s=96&v=4'
+        image: 'https://avatars3.githubusercontent.com/u/39841241?s=96&v=4'
         },
         {
-        codigo​: 2,
-        ​nombre​: 'Carlos Sneider Soto Sogamoso',
-        ​descripcion​: 'Soy Carlos Sneider Soto, estudiante de Ingeniería electrónica y telecomunicaciones  en la Universidad del Cauca, puedo comentar que en mi carrera he manejado algunos temas que se verán en el ciclo 3, además he manejado la metodología Scrum para desarrollar un sitio web usando php, http, javaScript y css.',
+        codigo: 2,
+        nombre: 'Carlos Sneider Soto Sogamoso',
+        descripcion: 'Soy Carlos Sneider Soto, estudiante de Ingeniería electrónica y telecomunicaciones  en la Universidad del Cauca, puedo comentar que en mi carrera he manejado algunos temas que se verán en el ciclo 3, además he manejado la metodología Scrum para desarrollar un sitio web usando php, http, javaScript y css.',
         rol: '(Main Backend Developer)',
-        image​: 'https://avatars3.githubusercontent.com/u/75052415?s=96&v=4'
+        image: 'https://avatars3.githubusercontent.com/u/75052415?s=96&v=4'
         },
         {
-        codigo​: 3,
-        ​nombre​: 'Harold Alejandro Caicedo',
-        ​descripcion​: 'Mi nombre es Alejandro Caicedo, soy estudiante de último semestre de ingeniería electrónica, durante mi carrera he tenido experiencia en el desarrollo y diseño de interfaces para aplicaciones móviles y páginas web. Tengo habilidades para la comunicación escrita y en cuanto al entorno de desarrollo del ciclo 3 tengo conocimientos de javascript. Finalmente tengo conocimiento en la gestión de proyectos para la innovación en perfiles educativos.',
-        image​: 'https://avatars0.githubusercontent.com/u/75107873?s=96&v=4'
+        codigo: 3,
+        nombre: 'Harold Alejandro Caicedo',
+        descripcion: 'Mi nombre es Alejandro Caicedo, soy estudiante de último semestre de ingeniería electrónica, durante mi carrera he tenido experiencia en el desarrollo y diseño de interfaces para aplicaciones móviles y páginas web. Tengo habilidades para la comunicación escrita y en cuanto al entorno de desarrollo del ciclo 3 tengo conocimientos de javascript. Finalmente tengo conocimiento en la gestión de proyectos para la innovación en perfiles educativos.',
+        rol: '(Frontend Developer)',
+        image: 'https://avatars0.githubusercontent.com/u/75107873?s=96&v=4'
         },
         {
-        codigo​: 4,
-        ​nombre​: 'Juan Sebastian Orduz',
-        ​descripcion​: 'Soy Ingeniero Electronico y Telecomunicaciones, me gradue el año pasado. Este 2020 estuve trabajando como desarrollador backend utilizando Django y DRF.',
+        codigo: 4,
+        nombre: 'Juan Sebastian Orduz',
+        descripcion: 'Soy Ingeniero Electronico y Telecomunicaciones, me gradue el año pasado. Este 2020 estuve trabajando como desarrollador backend utilizando Django y DRF.',
         rol: '(Backnd Developer)',
-        image​: 'https://avatars2.githubusercontent.com/u/75085664?s=96&v=4'
+        image: 'https://avatars2.githubusercontent.com/u/75085664?s=96&v=4'
         }
       ]
     }
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -3,11 +3,11 @@
         <div class="card badge-info container-fluid mt-5 pt-5" style="width: 18rem;">
             <img :src="member.image" class="card-img-top" :alt="member.codigo">
             <div class="card-body text-center">
-                <h3 class="card-title">{{member.nombres}}</h3>
+                <h3 class="card-title">{{member.codigo}}) {{member.nombre}}</h3>
                 <h6 class="card-text">{{member.rol}}</h6>
                 <hr>
                 <p class="text-justify">
-                    {{member.description}}
+                    {{member.descripcion}}
                 </p>
             </div>
         </div>
